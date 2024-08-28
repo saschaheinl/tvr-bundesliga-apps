03f7e54b55b0c9b4e2230e0442211c86f5a9aaee
Sascha Heinl <78321648+hnlvc@users.noreply.github.com>
2024-08-28 18:38:34 +0200

    changed deployment

--- API/TVR.Bundesliga.API.Domain/obj/Debug/net8.0/TVR.Bundesliga.API.Domain.AssemblyInfo.cs
+++ API/TVR.Bundesliga.API.Domain/obj/Debug/net8.0/TVR.Bundesliga.API.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TVR.Bundesliga.API.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28438f981976980587d10a3b23452fcb69542ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44525b2ee600e51af67cbbb176f29694ecf3ac20")]
 [assembly: System.Reflection.AssemblyProductAttribute("TVR.Bundesliga.API.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TVR.Bundesliga.API.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
