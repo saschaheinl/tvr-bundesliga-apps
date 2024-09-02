845a0ff43e1ff4e734fee9cddd0b52b4b05af034
Sascha Heinl <78321648+hnlvc@users.noreply.github.com>
2024-09-02 20:03:10 +0200

    cleanup and change of config

--- ticket-smasher/quasar.config.js
+++ ticket-smasher/quasar.config.js
@@ -19,6 +19,8 @@ module.exports = configure(function (ctx) {
   } else if (ctx.prod) {
     dotenvConfig({ path: '.env.prod' });
   }
+  
+  dotenvConfig({ path: envFilePath });
 
   console.log(ctx);
   return {
